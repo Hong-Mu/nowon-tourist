ef9f3d9e8154481822ae07e0f68e52aaf5beb0ac
eonpark <65460400+eonpark@users.noreply.github.com>
2023-11-22 02:21:38 +0900

    마이페이지 recyclerview 로직 추가

--- app/src/main/java/com/nowontourist/tourist/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/nowontourist/tourist/ui/mypage/MyPageFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import com.bumptech.glide.Glide
 import com.nowontourist.tourist.databinding.FragmentMyPageBinding
 import com.nowontourist.tourist.ui.auth.AuthHomeActivity
@@ -29,6 +30,20 @@ class MyPageFragment : Fragment() {
 
         initAuthButton()
         loadProfile()
+        initGalleryRecyclerView()
+    }
+
+    //지피티로 추가했는데 홍무 헬프
+    private fun initGalleryRecyclerView() {
+        val galleryAdapter = GalleryAdapter(getDummyPhotoList())
+        binding.recyclerViewGallery.apply {
+            layoutManager = GridLayoutManager(context, 2) // 여기서는 2열로 설정
+             adapter = galleryAdapter
+        }
+    }
+
+    private fun getDummyPhotoList(): List<String> {
+        TODO("Not yet implemented") // 실제데이터를 가져오는 로직
     }
 
     private fun loadProfile() {
